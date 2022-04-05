afa8fa9d52d69c7e049f2fe362274ad7ce75aa37
keevj <jhonatan72700606@gmail.com>
2022-04-05 19:10:17 -0400

    Styles screen Home

--- src/components/TabNavigator.js
+++ src/components/TabNavigator.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeStackScreen from './HomeStackScreen';
 import MyCondosStackScreen from './MyCondosStackScreen';
 import PerfilStackScreen from './PerfilStackScreen';
+import View from 'react-native';
+import Profile from '../assets/icons/home/profile';
+import Condo from '../assets/icons/home/my_condos';
+import Home from '../assets/icons/home/home';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name="Inicio" component={HomeStackScreen} />
-            <Tab.Screen name="Mis condominios" component={MyCondosStackScreen} />
-            <Tab.Screen name="Tab Perfil" component={PerfilStackScreen} />
-        </Tab.Navigator>
-    );
-}
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Inicio" component={HomeStackScreen} />
+      <Tab.Screen name="Mis condominios" component={MyCondosStackScreen} />
+      <Tab.Screen name="Perfil" component={PerfilStackScreen} />
+    </Tab.Navigator>
+  );
+};
 
 export default TabNavigation;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,29 +1,64 @@
-import React, { useContext } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { ActivityIndicator } from "react-native";
-import { AuthContext } from "../context/AuthContext";
+import React, {useContext} from 'react';
+import {Button, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {ActivityIndicator} from 'react-native';
+import {AuthContext} from '../context/AuthContext';
+import Logout from '../assets/icons/home/logout';
+import Notification from '../assets/icons/home/notification';
+import Emoji from '../assets/icons/home/emoji';
+import Comunicated from '../assets/icons/home/anuncios';
+import Expensive from '../assets/icons/home/expensas';
+import AreaCommon from '../assets/icons/home/areas-comunes';
+import Document from '../assets/icons/home/docs';
+import styles from '../styles/screens/HomeScreen.component.style';
 
 const HomeScreen = () => {
   const {userInfo, isLoading, logout} = useContext(AuthContext);
   return (
     <View style={styles.container}>
-      <ActivityIndicator size="large" color="#00ff00" animating={isLoading}/>
-      <Text style={styles.welcome}>Welcome {userInfo.current_user.name}</Text>
-      <Button title="Logout" color="red" onPress={logout}/>
-    </View> 
-  );
-}
+      {/* <Text style={styles.welcome}>Condominios {userInfo.current_user.name}</Text> */}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  welcome: {
-    fontSize: 18,
-    marginBottom: 8
-  }
-});
+      <View style={styles.header}>
+        <TouchableOpacity onPress={logout} style={styles.iconLogout}>
+          <Logout style={styles.icon} />
+        </TouchableOpacity>
+        <View style={styles.containerTitle}>
+          <Text style={styles.title}>Condominios</Text>
+          <Emoji />
+        </View>
+        <Notification style={styles.iconNotification} />
+      </View>
+
+      <View style={styles.containerCard}>
+        <View style={styles.card}>
+          <TouchableOpacity>
+            <Comunicated />
+            <Text style={styles.titleCard}>Comunicados</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.cardTwo}>
+          <TouchableOpacity>
+            <Expensive />
+            <Text style={styles.titleCard}>Expensas</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.containerCard}>
+        <View style={styles.card}>
+          <TouchableOpacity>
+            <AreaCommon />
+            <Text style={styles.titleCard}>Área común</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.cardTwo}>
+          <TouchableOpacity>
+            <Document />
+            <Text style={styles.titleCard}>Documentos</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <ActivityIndicator size="large" color="#00ff00" animating={isLoading} />
+    </View>
+  );
+};
 
 export default HomeScreen;
