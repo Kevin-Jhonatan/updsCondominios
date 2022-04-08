21074a4c8e46eccea329ac9f4950120fd0856b51
keevj <jhonatan72700606@gmail.com>
2022-04-08 18:22:50 -0400

    Finished styles documents

--- src/screens/document/DocumentScreen.js
+++ src/screens/document/DocumentScreen.js
@@ -1,10 +1,43 @@
 import React, {useContext} from 'react';
-import {Button, View, Text, TextInput} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
+import Back from '../../assets/icons/register/Icon_back';
+import Logo from '../../assets/icons/document/logo';
+import Word from '../../assets/icons/document/word';
+import Excel from '../../assets/icons/document/excel';
+import Pdf from '../../assets/icons/document/pdf';
+import styles from '../../styles/screens/document/DocumentScreen.component.styles';
 
 const DocumentScreen = ({navigation}) => {
   return (
-    <View>
-      <Text>Documentos</Text>
+    <View style={styles.container}>
+      <Back style={styles.back} />
+      <Text style={styles.title}>Documentos</Text>
+      <View style={styles.containerLogo}>
+        <Logo />
+      </View>
+      <ScrollView>
+        <View style={styles.card}>
+          <Word />
+          <View style={styles.containerText}>
+            <Text style={styles.titleDoc}>Horarios de áreas comunes</Text>
+            <Text style={styles.date}>25/03/2022</Text>
+          </View>
+        </View>
+        <View style={styles.card}>
+          <Pdf />
+          <View style={styles.containerText}>
+            <Text style={styles.titleDoc}>Horarios de áreas comunes</Text>
+            <Text style={styles.date}>25/03/2022</Text>
+          </View>
+        </View>
+        <View style={styles.card}>
+          <Excel />
+          <View style={styles.containerText}>
+            <Text style={styles.titleDoc}>Horarios de áreas comunes</Text>
+            <Text style={styles.date}>25/03/2022</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
